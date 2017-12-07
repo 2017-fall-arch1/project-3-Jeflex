58cd601087eb4957842e046e2240fa4a4969b2b6
pchassoun <pchassoun@miners.utep.edu>
2017-12-07 15:33:29 -0700

    here

--- shape-motion-demo/function.s
+++ shape-motion-demo/function.s
@@ -1,9 +1,8 @@
   .file "function.s"
 .text
   .arch msp430g2553
-
   .extern makeSong
 
-  makeSong:
-    mov #1500, R12
-    #call buzzer_set_period
+makeSong:
+  mov #1500, R12
+  CALL #buzzer_set_period
